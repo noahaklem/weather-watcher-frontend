fe162b0fd241daa6db773a24d0c0a17012ebc60e
Noah A Klem <noahaklem@gmail.com>
2022-03-01 16:17:01 -0700

    basic structure css for visual

--- src/App.js
+++ src/App.js
@@ -3,8 +3,18 @@ import React, { Component } from 'react';
 class App extends Component {
   render () {
     return (
-      <p>Holy Smokes</p>
-    )
+      <div className='app'>
+        <div className='navigation'>
+          <p>navigation</p>
+        </div>
+        <div className='todays-forecast'>
+          <p>todays weather</p>
+        </div>
+        <div className='future-forecast'>
+          <p>weather info</p>
+        </div>
+      </div>
+    );
   }
 }
 
