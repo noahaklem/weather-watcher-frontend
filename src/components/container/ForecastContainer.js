a2bfa2a046126f6b04a9b180fb87079749ff315b
Noah A Klem <noahaklem@gmail.com>
2022-03-04 20:03:32 -0700

    forecast classcomponent

---
+++ src/components/container/ForecastContainer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class ForecastContainer extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      forecasts: []
+    }
+  }
+
+
+  render(){
+    return
+  }
+}
